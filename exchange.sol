5271404329a54cbe2ca97588eed320a097b09998
AravindSarathChandraKumar <aravinds95@gmail.com>
2019-02-15 20:45:01 +0530

    Ether exchange contract added

---
+++ exchange.sol
@@ -0,0 +1,20 @@
+//smart contract to send ether
+pragma solidity 0.5.1;
+contract exchange{
+    
+    mapping(address => uint) public balance;
+    address payable  wallet;
+    
+    constructor (address payable _wallet) public{
+        
+        wallet= _wallet;
+    }
+    function buy() public payable{
+        
+        balance[msg.sender] += 1;
+        wallet.transfer(msg.value);
+        
+        
+        
+    }
+}
